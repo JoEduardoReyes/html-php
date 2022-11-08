1225c22a589d3dda0ffdefaec2d7f3944e5a5c6f
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-11-08 11:19:18 -0600

    PHP como preprocesador de HTML

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HTML desde PHP</title>
+</head>
+
+<body>
+  <h1>Hola desde PHP usando HTML</h1>
+  <p>
+    <?php
+    echo "Ahora esto se esta imprimiendo con PHP"
+    ?>
+  </p>
+</body>
+
+</html>
---
+++ php-prepro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP como preprocesador de HTML</title>
+</head>
+
+<body>
+  <h1>Hola! Esto es HTML desde PHP</h1>
+</body>
+
+</html>
